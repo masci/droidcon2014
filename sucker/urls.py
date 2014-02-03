0823802e9bba8561c6693587c8d2570fef24fb95
masci <mpippi@gmail.com>
2014-02-03 19:45:34 +0100

    lets do api

--- sucker/urls.py
+++ sucker/urls.py
@@ -1,1 +1,8 @@
+from rest_framework import routers
 
+from .views import TweetViewSet
+
+
+router = routers.DefaultRouter()
+router.register(r'tweets', TweetViewSet, base_name='tweets')
+urlpatterns = router.urls
--- sucker/views.py
+++ sucker/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import viewsets, filters
 
-# Create your views here.
+from .models import TweetMessage
+from .serializers import TweetMessageSerializer
+
+
+class TweetViewSet(viewsets.ReadOnlyModelViewSet):
+    serializer_class = TweetMessageSerializer
+
+    def get_queryset(self):
+        qs = TweetMessage.objects.all()
+
+        tweet_id = self.request.QUERY_PARAMS.get('id', None)
+        if tweet_id is not None:
+            qs = qs.filter(tweet_id__gt=tweet_id)
+
+        return qs
