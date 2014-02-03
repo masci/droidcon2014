12a735330eabcd0fde27e1c593dd26e3c40c257c
masci <mpippi@gmail.com>
2014-02-03 19:47:36 +0100

    added credits and license

--- sucker/twittersucker.py
+++ sucker/twittersucker.py
@@ -1,37 +1,20 @@
-sample = r"""
-{"created_at":"Sun Feb 02 21:41:11 +0000 2014","id":430093567592517632,"id_str":"430093567592517632","text":"@DroidRover go beyond","source":"\u003ca href=\"https:\/\/about.twitter.com\/products\/tweetdeck\" rel=\"nofollow\"\u003eTweetDeck\u003c\/a\u003e","truncated":false,"in_reply_to_status_id":null,"in_reply_to_status_id_str":null,"in_reply_to_user_id":2323779140,"in_reply_to_user_id_str":"2323779140","in_reply_to_screen_name":"DroidRover","user":{"id":25575225,"id_str":"25575225","name":"Massimiliano Pippi","screen_name":"maxpippi","location":"","url":"http:\/\/dev.pippi.im","description":"Software developer, Python lover, Dad.","protected":false,"followers_count":202,"friends_count":176,"listed_count":9,"created_at":"Fri Mar 20 21:00:09 +0000 2009","favourites_count":35,"utc_offset":3600,"time_zone":"Rome","geo_enabled":true,"verified":false,"statuses_count":880,"lang":"en","contributors_enabled":false,"is_translator":false,"is_translation_enabled":false,"profile_background_color":"022330","profile_background_image_url":"http:\/\/abs.twimg.com\/images\/themes\/theme1\/bg.png","profile_background_image_url_https":"https:\/\/abs.twimg.com\/images\/themes\/theme1\/bg.png","profile_background_tile":false,"profile_image_url":"http:\/\/pbs.twimg.com\/profile_images\/1504701775\/32_normal.jpg","profile_image_url_https":"https:\/\/pbs.twimg.com\/profile_images\/1504701775\/32_normal.jpg","profile_link_color":"0084B4","profile_sidebar_border_color":"A8C7F7","profile_sidebar_fill_color":"C0DFEC","profile_text_color":"333333","profile_use_background_image":false,"default_profile":false,"default_profile_image":false,"following":null,"follow_request_sent":null,"notifications":null},"geo":null,"coordinates":null,"place":null,"contributors":null,"retweet_count":0,"favorite_count":0,"entities":{"hashtags":[],"symbols":[],"urls":[],"user_mentions":[{"screen_name":"DroidRover","name":"DroidRover","id":2323779140,"id_str":"2323779140","indices":[0,11]}]},"favorited":false,"retweeted":false,"filter_level":"medium","lang":"en"}
-"""
-
-curl = """curl --request 'POST' 'https://stream.twitter.com/1.1/statuses/filter.json' --data 'track=droidrover' --header 'Authorization: OAuth oauth_consumer_key="qPFIo42gtwh9whOcWjZr6Q", oauth_nonce="136046c24893b4a6d9d09ce848fb1f70", oauth_signature="ygwUwmaaOeYPpIC8plenlbJ3phg%3D", oauth_signature_method="HMAC-SHA1", oauth_timestamp="1391387749", oauth_token="902555947-W9R96KP9MKNoVekzpc1t9a2W1SpvwCT5rZZIICcb", oauth_version="1.0"' --verbose"""
-curl += ' --silent --trace-ascii -'
-
-import json
-import subprocess
-import sys
-"""
-if __name__ == '__main__':
-
-    test_cmd = ['./simulate']
-    p = subprocess.Popen(curl, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True, close_fds=True)
-    while True:
-        sys.stdout.flush()
-
-        line = p.stdout.readline()
-        print line
-        
-        try:
-            data = json.loads(line)
-            print '--->', data['text'], data['id']
-        except ValueError:
-            pass
-
-        if line == '' and p.poll() != None:
-            break
-"""
-
-#!/usr/bin/python
 # -*- coding: utf-8 -*-
-    
+
+# Copyright (C) 2012 Gustav Arngården
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import time
 import pycurl
 import urllib
@@ -146,7 +129,7 @@ class TwitterStream:
                 print 'Got tweet with text: %s' % message.get('text')
 
 
-if __name__ == '__main__':
-    ts = TwitterStream()
-    ts.setup_connection()
-    ts.start()
+# if __name__ == '__main__':
+#     ts = TwitterStream()
+#     ts.setup_connection()
+#     ts.start()
