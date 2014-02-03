750ba4dfd1113088d25d438308013b29e419c183
masci <mpippi@gmail.com>
2014-02-03 22:38:06 +0100

    activate virtualenv before executing wsgi entrypoint

--- streamsucker/wsgi.py
+++ streamsucker/wsgi.py
@@ -8,6 +8,12 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
+VENV_DIR = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+
+# activate virtualenv
+activate_this = os.path.join(VENV_DIR, "bin", "activate_this.py")
+execfile(activate_this, dict(__file__=activate_this))
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "streamsucker.settings")
 
 from django.core.wsgi import get_wsgi_application
